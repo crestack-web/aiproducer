eafbb75eabdb251255adea9dea717702f4dd4398
business money <crestack@gmail.com>
2026-08-16 09:07:13 +0100

    Redirect /logo.svg and /icon.svg to Cloudinary brand mark

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
 
+const STUDIO_LOGO =
+  "https://res.cloudinary.com/dzjoqbg2u/image/upload/v1786866729/Untitled_-_August_15_2026_at_17.55.54-2_ipkio0.png";
+
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
+    ],
+  },
+  async redirects() {
+    return [
+      { source: "/logo.svg", destination: STUDIO_LOGO, permanent: false },
+      { source: "/icon.svg", destination: STUDIO_LOGO, permanent: false },
+      { source: "/favicon.ico", destination: STUDIO_LOGO, permanent: false },
+    ];
+  },
 };
 
 export default nextConfig;
