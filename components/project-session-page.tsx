126e34e0c732dd591c784e4935e9b17821158e01
business money <crestack@gmail.com>
2026-08-16 08:53:46 +0100

    Restore session page + Melody guide button

--- components/project-session-page.tsx
+++ components/project-session-page.tsx
@@ -1,1 +1,22 @@
-see_file
+"use client";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import Link from "next/link";
+import { useParams } from "next/navigation";
+import {
+  StudioPlayer,
+  CompactAudioPlayer,
+  RecordingVisualizer,
+  PlayerLoadingState,
+} from "@/components/studio-player";
+import { AppShell } from "@/components/app-shell";
+import {
+  SessionSteps,
+  sectionLabel,
+  isTaskOpen,
+  isTaskDone,
+  requiredOpen,
+  optionalOpen,
+} from "@/components/session-steps";
+import { ProjectSamplesPanel } from "@/components/project-samples-panel";
+
+export { default } from "./project-session-impl";
