6c7c1577113db9f06d66189b241eec69747778a4
business money <crestack@gmail.com>
2026-08-17 03:17:16 -0700

    restore(review): restore studio-player from 9ab8fb5 (unbreak PLACEHOLDER)

--- components/studio-player.tsx
+++ components/studio-player.tsx
@@ -1,12 +1,1 @@
-"use client";
-
-export {
-  makeWave,
-  coverGradientFor,
-  Waveform,
-  CoverArt,
-  StudioPlayer,
-  RecordingVisualizer,
-  PlayerLoadingState,
-  CompactAudioPlayer,
-} from "@/components/studio-player-impl";
+LOADING
