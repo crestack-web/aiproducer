1bb68cb5ddf89cc36c8d7b8b8154114b609ed232
business money <crestack@gmail.com>
2026-08-17 03:16:25 -0700

    restore(review): unbreak studio-player (re-export + impl)

--- components/studio-player.tsx
+++ components/studio-player.tsx
@@ -1,1 +1,12 @@
-PLACEHOLDER
+"use client";
+
+export {
+  makeWave,
+  coverGradientFor,
+  Waveform,
+  CoverArt,
+  StudioPlayer,
+  RecordingVisualizer,
+  PlayerLoadingState,
+  CompactAudioPlayer,
+} from "@/components/studio-player-impl";
